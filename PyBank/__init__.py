4c1c7192c228d65a71ed4ca2cd2a8efa1564b866
aLeX302-dev <alexandruchelariu302@gmail.com>
2025-02-02 15:31:18 +0200

    More Core Python

--- PyBank/__init__.py
+++ PyBank/__init__.py
@@ -1,3 +1,27 @@
+from enum import Enum
+
+class JobPerm(Enum):
+    Manager = 1
+    Operator = 2
+    Contabil = 3
+    Programator = 4
+    Secretar = 5
+    Asistenta = 6
+    Doctor = 7
+    Mecanic = 8
+
+class Utilitati:
+    @staticmethod
+    def print_banner(text):
+        lunigme_baner = 60
+        lungimea_textului = len(text)
+        numar_spatii_goale = (lunigme_baner - lungimea_textului -3)
+        print("-" * lunigme_baner)
+        print(f"| {text}" + " " * numar_spatii_goale + "|")
+        print("-" * lunigme_baner)
+
+
+
 class Person:
     def __init__(self, nume , prenume, cnp):
         self.nume = nume
@@ -6,16 +30,45 @@ class Person:
     def print_details(self):
         print(f"Nume: {self.nume} Prenume: {self.prenume} CNP {self.cnp}")
 
+class Employee(Person):
+    def __init__(self, nume , prenume, cnp, job):
+        super().__init__(nume , prenume, cnp)
+        self.job = job
+
+    def print_my_job(self):
+        print(f"Jobul acestui angajat este: {self.job}")
+
+
+
+
 class Bank:
     def __init__(self, bank_name):
         self.bank_name = bank_name
         self.archive = []
-    def register_employee(self, person):
+        self.jobs_available = [JobPerm.Manager, JobPerm.Operator, JobPerm.Contabil, JobPerm.Programator,
+                               JobPerm.Secretar, JobPerm.Asistenta, JobPerm.Doctor, JobPerm.Mecanic]
+
+    def check_jobs(self,employee):
+        if employee.job in self.jobs_available:
+            return True
+        else:
+            return False
+            Utilitati.print_banner(f"Job not available {empolyee.job}")
+
+    def check_employee(self,employee):
+        if not self.check_jobs(employee):
+            return False
         for existing_person in self.archive:
-            if person.cnp == existing_person.cnp:
-                print("Error: Existing Person can't be added")
-                return
-        self.archive.append(person)
+            if employee.cnp == existing_person.cnp:
+                Utilitati.print_banner("Error: Existing Person can't be added")
+                return False
+        return True
+
+
+    def register_employee(self,employee):
+        if self.check_employee(employee):
+            self.archive.append(employee)
+
     def unregister_employee(self, cnp):
         for p in self.archive:
             if p.cnp == cnp:
@@ -24,6 +77,9 @@ class Bank:
     def print_all_employee(self):
         for i in self.archive:
             i.print_details()
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -2,25 +2,33 @@ from PyBank import *
 
 
 bank = Bank("Banca Transilvania")
+employee_1 = Employee("Popescu", "Ion", "1234567890123", JobPerm.Manager)
+bank.register_employee(employee_1)
 
-persoana_1 = Person("Ionescu", "Cristi", 123456789)
-persoana_2 = Person("Popescu", "Maria", 987654321)
-persoana_3 = Person("Vasilescu", "Ion", 456789123)
-persoana_4 = Person("Georgescu", "Elena", 321654987)
-persoana_5 = Person("Mihaescu", "Marin", 654987321)
-persoana_6 = Person("Mihaescu", "Marin", 654987321)
+employee_2 = Employee("Ionescu", "Maria", "9876543210987", JobPerm.Operator)
+bank.register_employee(employee_2)
+
+employee_3 = Employee("Vasilescu", "Ion", 456789123,JobPerm.Operator)
+employee_4 = Employee("Georgescu", "Elena", 321654987,JobPerm.Operator)
+employee_5 = Employee("Mihaescu", "Marin", 654987321,JobPerm.Operator)
+employee_6 = Employee("Mihaescu", "Marin", 654987321,JobPerm.Operator)
 
 
-bank.register_employee(persoana_1)
-bank.register_employee(persoana_2)
-bank.register_employee(persoana_3)
-bank.register_employee(persoana_4)
-bank.register_employee(persoana_5)
-bank.register_employee(persoana_6)
+
+
+bank.register_employee(employee_3)
+bank.register_employee(employee_4)
+bank.register_employee(employee_5)
+bank.register_employee(employee_6)
 
 bank.print_all_employee()
 print("---")
-bank.unregister_employee(persoana_3.cnp)
+bank.unregister_employee(employee_3.cnp)
 
 bank.print_all_employee()
 print("---")
+
+
+
+
+employee_1 = Employee("Ionescu", "Cristian",3216123547,JobPerm.Operator)
